ec18cd7f79e865b34f39f5efb4a87e2a62f6bff4
Hames.Al-Sharoa <159185115+Hames-al-sharoa@users.noreply.github.com>
2024-05-08 01:24:18 +0000

    save all

--- js/script.js
+++ js/script.js
@@ -1,5 +1,19 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
+// Copyright (c) 2024 hames.al-sharoa All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: hames.al-sharoa
+// Created on: Apr 2024
 // This file contains the JS functions for index.html
+"use strict"
+
+function button() {
+
+  const lengththeAString = parseInt(document.getElementById("lengthA").value)
+  const lengththeBString = parseInt(document.getElementById("lengthB").value)
+  const lengththeCString = parseInt(document.getElementById("lengthC").value)
+
+  const lengthA = parseFloat(lengththeAString)
+  const lengthB = parseFloat(lengththeBString)
+  const lengthC = parseFloat(lengththeCString)
+
+  / using the cosine law
+  const andleA = Math.acos((lengthB ** 2 + lengthC ** 2 - lengthA ** 2) / (2 *))
