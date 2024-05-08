c94da978aa5f4dacd3cc415e7e644afa3fd30836
Hames.Al-Sharoa <159185115+Hames-al-sharoa@users.noreply.github.com>
2024-05-08 18:01:58 +0000

    save changes

--- js/script.js
+++ js/script.js
@@ -15,5 +15,23 @@ function button() {
   const lengthB = parseFloat(lengththeBString)
   const lengthC = parseFloat(lengththeCString)
 
-  / using the cosine law
-  const andleA = Math.acos((lengthB ** 2 + lengthC ** 2 - lengthA ** 2) / (2 *))
+  // using the cosine law
+  const angleA = Math.acos((lengthB ** 2 + lengthC ** 2 - lengthA ** 2) / (2 * lengthB * lengthC)) * (180 / Math.PI)
+  const angleB = Math.acos((lengthC ** 2 + lengthA ** 2 - lengthB ** 2) / (2 * lengthC * lengthA)) * (180 / Math.PI)
+  const angleC = Math.acos((lengthA ** 2 + lengthB ** 2 - lengthC ** 2) / (2 * lengthA * lengthB)) * (180 / Math.PI)
+  
+  if (angleA == angleB && angleA == angleC) {
+    document.getElementById("math").innerHTML =
+    "You have an equilateral triangle!" 
+  }  else if (
+    (angleA != angleB && angleC) ||
+    (angleA != angleB && angleC) 
+  ) {
+    document.getElementById("math").innerHTML =
+    "You have an isosceles triangle!"
+  } else {
+    document.getElementById("math").innerHTML =
+    "this triangle doesn't exist."
+  }
+}
+  
